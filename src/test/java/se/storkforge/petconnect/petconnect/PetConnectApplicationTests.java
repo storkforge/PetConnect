aa556745bcbeca3e1bc6980b764769a300a9fd77
DPatti88 <patricia.nilsson@iths.se>
2025-03-19 11:45:19 +0100

    Initial commit

---
+++ src/test/java/se/storkforge/petconnect/petconnect/PetConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package se.storkforge.petconnect.petconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
