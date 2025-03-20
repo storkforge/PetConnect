a51b318042c584715bd69282c446590b7c9ca1f5
MarcGust <marcus.gustavsson@iths.se>
2025-03-20 11:47:35 +0100

    add Pet entity class

---
+++ src/main/java/entity/Pet.java
@@ -0,0 +1,116 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String species;
+    private boolean pedigree;
+    private int age;
+    private String owner;
+    private String location;
+
+    public Pet() {}
+
+    public Pet(String name, String species, boolean pedigree, int age, String owner, String location) {
+        this.name = name;
+        this.species = species;
+        this.pedigree = pedigree;
+        this.age = age;
+        this.owner = owner;
+        this.location = location;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public boolean getPedigree() {
+        return pedigree;
+    }
+
+    public void setPedigree(boolean pedigree) {
+        this.pedigree = pedigree;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", species='" + species + '\'' +
+                ", pedigree=" + (pedigree ? "Yes" : "No") +
+                ", age=" + age +
+                ", owner='" + owner + '\'' +
+                ", location='" + location + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pet pet = (Pet) o;
+        return id != null && id.equals(pet.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
