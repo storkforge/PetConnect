bdd75c2689314a27ec3236d8471a43230e84e50a
Evgeniia Granina <evgeniia.granina@iths.se>
2025-03-25 08:30:17 +0100

    Add Meet up class

---
+++ src/main/java/se/storkforge/petconnect/entity/MeetUp.java
@@ -0,0 +1,90 @@
+package se.storkforge.petconnect.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class MeetUp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String location;
+    private LocalDateTime dateTime;
+
+    @ManyToMany
+    private List<User> participants;
+
+    private String status; // "PLANNED", "CONFIRMED", "CANCELED"
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public List<User> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(List<User> participants) {
+        this.participants = participants;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "MeetUp{" +
+                "id=" + id +
+                ", location='" + location + '\'' +
+                ", dateTime=" + dateTime +
+                ", participants=" + participants +
+                ", status='" + status + '\'' +
+                '}';
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        MeetUp meetUp = (MeetUp) o;
+        return getId() != null && Objects.equals(getId(), meetUp.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
