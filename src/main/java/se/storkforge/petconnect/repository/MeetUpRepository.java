7a0da432ace28639af7f19d6ec50a3704b5a1304
Evgeniia Granina <evgeniia.granina@iths.se>
2025-03-25 08:15:34 +0100

    Add Meet up repository

---
+++ src/main/java/se/storkforge/petconnect/repository/MeetUpRepository.java
@@ -0,0 +1,15 @@
+package se.storkforge.petconnect.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import se.storkforge.petconnect.entity.MeetUp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface MeetUpRepository extends JpaRepository<MeetUp, Long> {
+
+//    List<MeetUp> findByLocationContaining(String location);
+//    List<MeetUp> findByDateTimeBetween(LocalDateTime start, LocalDateTime end);
+}
