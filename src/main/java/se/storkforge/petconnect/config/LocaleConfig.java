100c5d26a77cdbb6cf7cfa62ba1f3bdb04806484
Linus Holmlund <Linus_holmlund@outlook.com>
2025-03-25 14:51:18 +0100

    removed unnecessary imports

--- src/main/java/se/storkforge/petconnect/config/LocaleConfig.java
+++ src/main/java/se/storkforge/petconnect/config/LocaleConfig.java
@@ -1,12 +1,10 @@
 package se.storkforge.petconnect.config;
 
-import org.springframework.cglib.core.Local;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
 
 import java.util.List;
 import java.util.Locale;
