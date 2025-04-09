50c0378527c280e265535b2bc00b2178bc2d2c09
Linus Holmlund <Linus_holmlund@outlook.com>
2025-04-09 16:17:56 +0200

    integrated EnvInitializer early in SpringApplication to inject dotenv variables before startup

--- src/main/java/se/storkforge/petconnect/PetConnectApplication.java
+++ src/main/java/se/storkforge/petconnect/PetConnectApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import se.storkforge.petconnect.config.EnvInitializer;
 
 @SpringBootApplication
 @EntityScan("se.storkforge.petconnect.entity")
@@ -11,6 +12,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class PetConnectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PetConnectApplication.class, args);
+        SpringApplication app = new SpringApplication(PetConnectApplication.class);
+        app.addInitializers(new EnvInitializer());
+        app.run(args);
     }
+
 }
