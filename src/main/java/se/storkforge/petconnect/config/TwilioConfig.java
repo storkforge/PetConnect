e95d438848896b9086c168fabff29286035e5924
Linus Holmlund <Linus_holmlund@outlook.com>
2025-04-16 02:50:16 +0200

    Updated TwilioConfig to use constructor injection and profile-based activation

--- src/main/java/se/storkforge/petconnect/config/TwilioConfig.java
+++ src/main/java/se/storkforge/petconnect/config/TwilioConfig.java
@@ -2,20 +2,23 @@ package se.storkforge.petconnect.config;
 
 import com.twilio.Twilio;
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 @Configuration
+@Profile("!dev")
 public class TwilioConfig {
 
-    @Value("${twilio.account_sid}")
-    private String accountSid;
+    private final String accountSid;
+    private final String authToken;
+    private final String fromPhoneNumber;
 
-    @Value("${twilio.auth_token}")
-    private String authToken;
-
-    @Value("${twilio.phone_number}")
-    private String fromPhoneNumber;
+    public TwilioConfig(String accountSid, String authToken, String fromPhoneNumber) {
+        this.accountSid = accountSid;
+        this.authToken = authToken;
+        this.fromPhoneNumber = fromPhoneNumber;
+    }
 
     @PostConstruct
     public void init() {
