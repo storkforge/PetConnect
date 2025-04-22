28c68199273e8f59e36b3e2f0578f42008135a2b
Alexander Pettersson <alexpet5888@gmail.com>
2025-04-22 19:23:04 +0200

    Refactor

--- src/main/java/se/storkforge/petconnect/controller/UserProfileController.java
+++ src/main/java/se/storkforge/petconnect/controller/UserProfileController.java
@@ -1,7 +1,5 @@
 package se.storkforge.petconnect.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,7 +12,6 @@ import se.storkforge.petconnect.service.PetService;
 import se.storkforge.petconnect.service.UserService;
 import se.storkforge.petconnect.service.storageService.FileStorageService;
 import java.security.Principal;
-import java.util.List;
 import java.util.Set;
 
 @Controller
@@ -23,14 +20,12 @@ public class UserProfileController {
 
     private final UserService userService;
     private final PetService petService;
-    private final FileStorageService fileStorageService;
 
     public UserProfileController(UserService userService,
                                  PetService petService,
                                  FileStorageService fileStorageService) {
         this.userService = userService;
         this.petService = petService;
-        this.fileStorageService = fileStorageService;
     }
 
     @GetMapping("/{username}")
